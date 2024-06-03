7b8b4dddb7ba843c1cd6068bff6de841b909fcab
Christopher Caldwell <christopher.caldwell@eltako.de>
2024-06-03 14:07:02 +0200

    Fix adding of provisional scnes cluster to matter device types
    This fix issues the following matter device types:
    
    on off light
    dimmable light
    color temperature light
    extended color light
    
    on off plugin unit
    dimmable plugin unit
    
    thermostat
    
    window covering device

--- components/esp_matter/esp_matter_endpoint.h
+++ components/esp_matter/esp_matter_endpoint.h
@@ -147,7 +147,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::on_off::config_t on_off;
 } config_t;
 
@@ -162,7 +161,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::on_off::config_t on_off;
     cluster::level_control::config_t level_control;
 } config_t;
@@ -178,7 +176,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::on_off::config_t on_off;
     cluster::level_control::config_t level_control;
     cluster::color_control::config_t color_control;
@@ -195,7 +192,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::on_off::config_t on_off;
     cluster::level_control::config_t level_control;
     cluster::color_control::config_t color_control;
@@ -264,7 +260,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::on_off::config_t on_off;
 } config_t;
 
@@ -279,7 +274,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::on_off::config_t on_off;
     cluster::level_control::config_t level_control;
 } config_t;
@@ -308,7 +302,6 @@ namespace thermostat {
 typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
-    cluster::scenes::config_t scenes;
     cluster::groups::config_t groups;
     cluster::thermostat::config_t thermostat;
 } config_t;
@@ -422,7 +415,6 @@ typedef struct config {
     cluster::descriptor::config_t descriptor;
     cluster::identify::config_t identify;
     cluster::groups::config_t groups;
-    cluster::scenes::config_t scenes;
     cluster::window_covering::config_t window_covering;
     config(uint8_t end_product_type = 0) : window_covering(end_product_type) {}
 } config_t;
